efdc45a6aa9966b705343df11550b4622837d5e3
JaksaRiger <jaksa.riger@gmail.com>
2022-11-08 17:35:37 +0100

    file update

--- suma_prvih_n.py
+++ suma_prvih_n.py
@@ -1,21 +1,10 @@
-# n = int(input("Unesi broj n: "))
+n = int(input("Unesi broj n: "))
 
 suma = 0
 i = 1
 
-# while i <= n:
-#     suma = suma + i
-#     i = i + 1
+while i <= n:
+    suma = suma + i
+    i = i + 1
 
-range(5) # 0 1 2 3 4
-range (2, 5)  # 2 3 4
-range(1, 10, 2) # 1 3 5 7 9 
-
-# for x in range(1, n+1):
-#     print(x)
-#     suma = suma + x
-
-for i in range(0, 10, 3):
-    print(i)
-
-# print(suma)
+print(suma)
